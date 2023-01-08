335c93fa44c927dd4ae3695a845969b51f49c2a6
Ozzy Tasci <oguzcantasci@gmail.com>
2023-01-08 15:53:58 -0500

    Add route handle for the shortURL form submission

--- express-server.js
+++ express-server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-
-
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+const generateRandomString = function(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,6 +25,13 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  const shortURL = generateRandomString(6);
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
