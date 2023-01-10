d66e58e569fbad1e13dffa168c738521d4b6ec4c
Ozzy Tasci <oguzcantasci@gmail.com>
2023-01-09 20:43:17 -0500

    Implement cookies for username

--- express-server.js
+++ express-server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
@@ -24,7 +28,18 @@ const urlDatabase = {
 
 // Route handler to show shortURL submision form
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
+});
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 // Route handler for handling the shortURL submission
@@ -37,7 +52,7 @@ app.post("/urls", (req, res) => {
 
 // Route handler to show all URLs
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies['username'], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
@@ -49,7 +64,7 @@ app.get("/u/:id", (req, res) => {
 
 // Route handler to show newly created shortURL and the corresponding longURL
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { username: req.cookies['username'], id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
